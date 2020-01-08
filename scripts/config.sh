7c12d55cee84b6b796ddbd258606728b84f9cf68
Peter Weissig <peterweissig@github.com>
2020-01-09 00:44:31 +0100

    implemented function server_config_git_add_user

--- scripts/config.sh
+++ scripts/config.sh
@@ -91,9 +91,9 @@ function server_config_aptcacher_restore() {
 #***************************[git_repo]****************************************
 # 2020 01 05
 
-alias server_config_git_add_user="echo '...todo...'"
 alias server_config_git_create_repo="echo '...todo...'"
 
+# 2020 01 05
 function server_config_git_init() {
 
     temp="sets up basic configurations to add git repos."
@@ -245,6 +245,129 @@ function server_config_git_init_restore() {
         echo "removing symbolic link $official_storage_path"
         sudo rm "$official_storage_path"
     fi
+
+    echo "done :-)"
+}
+
+# 2020 01 08
+function server_config_git_add_user() {
+
+    temp="updates the git-user to add one more user for the git repos."
+    git_home="/home/git/"
+    git_ssh="${git_home}.ssh/"
+    FILENAME_CONFIG="${git_ssh}authorized_keys"
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME [<public-key-file>]"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 0-1 parameters"
+        echo "    [#1:]optional path for public key file"
+        echo "         e.g. ~/.ssh/id_rsa.pub"
+        echo "This function $temp"
+        echo "If path for key file is not set (e.g. \"\"), then the key"
+        echo "must be entered manually when prompted."
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -gt 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # check for user agreement
+    _config_simple_parameter_check "$FUNCNAME" "" "$temp"
+    if [ $? -ne 0 ]; then return -1; fi
+
+    # check if git-user exists
+    git_user="$(cat /etc/passwd | grep -e "^git")"
+    if [ "$git_user" == "" ] || [ ! -d "$git_home" ]; then
+        echo "$FUNCNAME: git-user or its home does not exist."
+        echo "  Did you call server_config_git_init ?"
+        return -2
+    fi
+
+    # load key
+    key=""
+    if [ "$1" != "" ]; then
+        # check if key file exists
+        if [ ! -e "$1" ]; then
+            echo "$FUNCNAME: key file \"$1\" does not exist."
+            return -3
+        fi
+
+        # load from key file
+        key="$(cat "$1")"
+        if [ $? -ne 0 ]; then return -4; fi
+    else
+        echo "Need a public key file for the user."
+        echo "e.g. \"ssh-rsa AAAANzaC1yc2E...AvJICUvax2T9va5 egon@panther\""
+        echo "Please paste the key file content and press enter:"
+        echo -n "  "
+        read key
+        if [ $? -ne 0 ]; then return -5; fi
+    fi
+
+    # check key
+    if [ "$key" == "" ]; then
+        echo "$FUNCNAME: empty key."
+        return -6
+    fi
+
+    if [ "${key:0:8}" != "ssh-rsa " ]; then
+        echo "$FUNCNAME: key does not start with \"rsa-ssh\"."
+        return -6
+    fi
+
+    # append key
+    temp="no-port-forwarding,no-X11-forwarding"
+    key_full="$temp,no-agent-forwarding,no-pty $key"
+
+    # create ssh folder
+    if sudo [ ! -d "$git_ssh" ]; then
+        sudo mkdir -p  "$git_ssh" && \
+        sudo chmod 700 "$git_ssh" && \
+        sudo chown git "$git_ssh"
+    fi
+    if [ $? -ne 0 ]; then return -7; fi
+
+    # create keyfile
+    if sudo [ ! -e "$FILENAME_CONFIG" ]; then
+        sudo touch     "$FILENAME_CONFIG" && \
+        sudo chmod 600 "$FILENAME_CONFIG" && \
+        sudo chown git "$FILENAME_CONFIG"
+    fi
+    if [ $? -ne 0 ]; then return -7; fi
+
+    # load current file
+    current_keys="$(sudo cat "$FILENAME_CONFIG")"
+    if [ $? -ne 0 ]; then return -8; fi
+
+    # check if key is already there
+    temp="$(echo "$current_keys" | grep "$key" | wc -w )"
+    if [ "$temp" -ne 0 ]; then
+        echo "$FUNCNAME: key is already set."
+        return -8
+    fi
+
+    # add key
+    AWK_STRING="
+        { print \$0 }
+
+        # add public user key to git-user
+        END {
+            print \"$key_full\"
+        }
+    "
+    _config_file_modify_full "$FILENAME_CONFIG" "git-user" \
+      "$AWK_STRING" "normal" "" "sudo"
+    if [ $? -ne 0 ]; then return -9; fi
 
     echo "done :-)"
 }
