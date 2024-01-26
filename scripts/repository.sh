d14578b364d86b2b0e48e396b32b663530750284
Peter Weissig <peterweissig@github.com>
2024-01-26 22:49:15 +0100

    minor fix (do not export repository functions if bash/repo was already sourced)

--- scripts/repository.sh
+++ scripts/repository.sh
@@ -6,6 +6,7 @@
 # SERVER_PATH_WORKSPACE
 
 
+
 #***************************[server]******************************************
 # 2019 12 01
 
@@ -13,41 +14,45 @@
 export REPO_ROOT_GITHUB_URL="https://github.com/peterweissig/"
 
 
+
 #***************************[bash]********************************************
-# 2023 11 18
+# 2024 01 26
 
-# paths
-# note: this is already set in file bash/repo/list.sh
-export REPO_BASH_PATH="${SERVER_PATH_WORKSPACE}bash/"
-export REPO_BASH_MASTER_PATH="${REPO_BASH_PATH}master/"
+if [ "$SOURCED_BASH_REPO" == "" ]; then
+  # paths
+  # note: this is already set in file bash/repo/list.sh
+  export REPO_BASH_PATH="${SERVER_PATH_WORKSPACE}bash/"
+  export REPO_BASH_MASTER_PATH="${REPO_BASH_PATH}master/"
 
-# repos
-# note: this is already set in file bash/repo/list.sh
-export REPO_BASH_REPO=("${REPO_BASH_PATH}repo/" \
-  "${REPO_ROOT_GITHUB_URL}bash_repo.git")
+  # repos
+  # note: this is already set in file bash/repo/list.sh
+  export REPO_BASH_REPO=("${REPO_BASH_PATH}repo/" \
+    "${REPO_ROOT_GITHUB_URL}bash_repo.git")
 
-function git_clone_bash_repo()  { _repo_git_clone ${REPO_BASH_REPO[*]};  }
-function git_pull_bash_repo()   { _repo_git_pull "${REPO_BASH_REPO[0]}"; }
-function git_push_bash_repo()   { _repo_git_push "${REPO_BASH_REPO[0]}"; }
-function git_status_bash_repo() { _repo_git_st   "${REPO_BASH_REPO[0]}"; }
+  function git_clone_bash_repo()  { _repo_git_clone ${REPO_BASH_REPO[*]};  }
+  function git_pull_bash_repo()   { _repo_git_pull "${REPO_BASH_REPO[0]}"; }
+  function git_push_bash_repo()   { _repo_git_push "${REPO_BASH_REPO[0]}"; }
+  function git_status_bash_repo() { _repo_git_st   "${REPO_BASH_REPO[0]}"; }
 
-# main scripts
-# note: this is already set in file bash/repo/list.sh
-export REPO_BASH_MASTER_SERVER=("${REPO_BASH_MASTER_PATH}server/" \
-  "${REPO_ROOT_GITHUB_URL}bash_master_server.git")
+  # main scripts
+  # note: this is already set in file bash/repo/list.sh
+  export REPO_BASH_MASTER_SERVER=("${REPO_BASH_MASTER_PATH}server/" \
+    "${REPO_ROOT_GITHUB_URL}bash_master_server.git")
 
-function git_clone_bash_master_server() {
-    _repo_git_clone ${REPO_BASH_MASTER_SERVER[*]}
-}
-function git_pull_bash_master_server() {
-    _repo_git_pull "${REPO_BASH_MASTER_SERVER[0]}"
-}
-function git_push_bash_master_server() {
-    _repo_git_push "${REPO_BASH_MASTER_SERVER[0]}"
-}
-function git_status_bash_master_server() {
-    _repo_git_st "${REPO_BASH_MASTER_SERVER[0]}"
-}
+  function git_clone_bash_master_server() {
+      _repo_git_clone ${REPO_BASH_MASTER_SERVER[*]}
+  }
+  function git_pull_bash_master_server() {
+      _repo_git_pull "${REPO_BASH_MASTER_SERVER[0]}"
+  }
+  function git_push_bash_master_server() {
+      _repo_git_push "${REPO_BASH_MASTER_SERVER[0]}"
+  }
+  function git_status_bash_master_server() {
+      _repo_git_st "${REPO_BASH_MASTER_SERVER[0]}"
+  }
+fi
+
 
 
 #***************************[global update and stat]**************************
@@ -55,3 +60,4 @@ function git_status_bash_master_server() {
 
 function server_repo_update() { repo_pull_all;   }
 function server_repo_status() { repo_status_all; }
+
